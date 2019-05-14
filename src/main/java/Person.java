63a0fc02a7588c04b67cedc3ed1a731d981be19b
Theophelus <aneletom10@gmail.com>
2019-05-14 09:36:05 +0200

    defining a Person class with its propertires and get methods

---
+++ src/main/java/Person.java
@@ -0,0 +1,22 @@
+public class Person {
+
+    private String firstName, lastName, email;
+
+    Person(String firstName, String lastName, String email){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
